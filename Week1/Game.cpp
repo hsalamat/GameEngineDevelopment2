f6f2005b278d470247a7feb138d341ca1560256a
hsalamat <hooman_salamat@yahoo.com>
2021-01-11 19:18:26 -0500

    update

---
+++ Week1/Game.cpp
@@ -0,0 +1,98 @@
+#include <Book/Game.hpp>
+
+
+Game::Game()
+#pragma region Step2
+	: mWindow(sf::VideoMode(800, 600), "SFML Application", sf::Style::Close)
+	, mTexture()
+	, mBackground()
+	, mPlayer()
+	, mMusic()
+	, mFont()
+	, mIcon()
+#pragma endregion
+
+{
+
+	mPlayer.setRadius(40.0f);
+	mPlayer.setPosition(100.f, 100.f);
+	mPlayer.setFillColor(sf::Color::Cyan);
+
+#pragma region step3
+
+
+	mIcon.loadFromFile("Media/Textures/icon.png");
+	mWindow.setIcon(mIcon.getSize().x, mIcon.getSize().y, mIcon.getPixelsPtr());
+
+	//Load a sprite to display
+	if (!mTexture.loadFromFile("Media/Textures/cute_image.jpg"))
+		return;
+	mBackground.setTexture(mTexture);
+
+	if (!mFont.loadFromFile("Media/Sansation.ttf"))
+		return;
+
+
+	mText.setString("Hello SFML");
+	mText.setFont(mFont);
+	mText.setPosition(5.f, 5.f);
+	mText.setCharacterSize(50);
+	mText.setFillColor(sf::Color::Black);
+
+	//mText.setRotation(45);
+
+	
+	mMusic.openFromFile("Media/Textures/nice_music.ogg");
+	//Play the music
+	mMusic.play();
+
+#pragma endregion
+
+}
+
+void Game::run()
+{
+
+	while (mWindow.isOpen())
+	{
+		processEvents();
+		update();
+		render();
+	}
+}
+
+void Game::processEvents()
+{
+	sf::Event event;
+	while (mWindow.pollEvent(event))
+	{
+		switch (event.type)
+		{
+		case sf::Event::Closed:
+			mWindow.close();
+			break;
+		}
+	}
+}
+
+void Game::update()
+{
+}
+
+void Game::render()
+{
+	mWindow.clear();
+
+#pragma region Step4
+
+	mWindow.draw(mBackground);
+	//Draw the string
+	mWindow.draw(mText);
+#pragma endregion
+
+	//Update the window
+	mWindow.display();
+}
+
+
+
---
+++ Week1/Include/Book/Game.hpp
@@ -0,0 +1,41 @@
+#ifndef BOOK_GAME_HPP
+#define BOOK_GAME_HPP
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+
+class Game : private sf::NonCopyable
+{
+public:
+	Game();
+	void					run();
+
+
+private:
+	void					processEvents();
+	void					update();
+	void					render();
+
+
+private:
+
+	sf::RenderWindow		mWindow;
+	sf::CircleShape			mPlayer;
+	sf::Sprite				mBackground;
+	sf::Texture				mTexture;
+
+#pragma region Step 1
+	//Set the Icon
+	sf::Image mIcon;
+
+	//Create a graphical text to display
+	sf::Font mFont;
+	sf::Text mText;
+
+	sf::Music mMusic;
+#pragma endregion
+
+};
+
+#endif // BOOK_GAME_HPP
