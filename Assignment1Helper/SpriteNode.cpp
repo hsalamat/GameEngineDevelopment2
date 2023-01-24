3370123c729f804f4004d5cf8447bc8194cb2365
Hooman Salamat <hooman_salamat@yahoo.com>
2023-01-24 14:09:07 -0500

    Assignment1 Helper

---
+++ Assignment1Helper/SpriteNode.cpp
@@ -0,0 +1,29 @@
+#include "SpriteNode.h"
+#include "Game.hpp"
+
+SpriteNode::SpriteNode(Game* game) : Entity(game)
+{
+}
+
+void SpriteNode::drawCurrent() const
+{
+	renderer->World = getTransform();
+	renderer->NumFramesDirty++;
+}
+
+void SpriteNode::buildCurrent()
+{
+	auto render = std::make_unique<RenderItem>();
+	renderer = render.get();
+	renderer->World = getTransform();
+	XMStoreFloat4x4(&renderer->TexTransform, XMMatrixScaling(10.0f, 10.0f, 10.0f));
+	renderer->ObjCBIndex = game->getRenderItems().size();
+	renderer->Mat = game->getMaterials()["Desert"].get();
+	renderer->Geo = game->getGeometries()["boxGeo"].get();
+	renderer->PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	renderer->IndexCount = renderer->Geo->DrawArgs["box"].IndexCount;
+	renderer->StartIndexLocation = renderer->Geo->DrawArgs["box"].StartIndexLocation;
+	renderer->BaseVertexLocation = renderer->Geo->DrawArgs["box"].BaseVertexLocation;
+
+	game->getRenderItems().push_back(std::move(render));
+}
